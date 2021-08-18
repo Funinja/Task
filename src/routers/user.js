4d4a80417b2140a4296aa594f2aebed1da0684c1
Dennis lam <denwt.lam@outlook.com>
2021-08-18 15:26:24 -0400

    add token authenication to patch and post user API calls

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,23 +10,6 @@ router.get('/users/me', auth, async (req, res) => {
 
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    }catch(e) {
-        res.status(500).send();
-    }
-
-});
-
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
@@ -81,7 +64,7 @@ router.post('/users/logoutAll', auth, async(req, res) => {
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
 
     const updates = Object.keys(req.body);
 
@@ -95,18 +78,14 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id);
 
         updates.forEach((update) => {
-            user[update] = req.body[update];
+            req.user[update] = req.body[update];
         })
 
-        await user.save();
+        await req.user.save();
 
-        if(!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
+        res.send(req.user);
 
     }catch(e) {
 
@@ -115,15 +94,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
+        await req.user.remove();
 
-        if(!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
+        res.send(req.user);
     }catch (e) {
         res.status(500).send();
     }
